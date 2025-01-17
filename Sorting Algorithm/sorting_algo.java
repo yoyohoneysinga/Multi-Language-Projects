37fabf7e6abef5ab286473f0366e5c53e8c53bb0
yoyohoneysinga <jmail3233@gmail.com>
2025-01-16 16:48:15 -0800

    Added Python and Java implementations

--- Sorting Algorithm/sorting_algo.java
+++ Sorting Algorithm/sorting_algo.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+
 public class sorting_algo {
     
+    private static boolean is_sorted(int[] array){
+        for (int i = 0; i < array.length - 1; i++){
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        int[] array = {23, 5, 87, 12, 41, 3, 59, 74, 18, 62};
+        while (!is_sorted(array)) {
+            for (int i = 0; i < array.length - 1; i++){
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
 }
---
+++ Sorting Algorithm/sorting_algo.py
@@ -0,0 +1,16 @@
+def is_sorted(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            return False
+    return True
+
+array = [23, 5, 87, 12, 41, 3, 59, 74, 18, 62]
+
+while not is_sorted(array):
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            temp = array[i]
+            array[i] = array[i + 1]
+            array[i + 1] = temp
+
+print(array)
